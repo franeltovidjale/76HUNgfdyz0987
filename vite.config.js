1868e29845a050b7e80bfe1917b3dc578fb31701
franeltovidjale <tovidjalef@gmail.com>
2024-11-25 21:16:33 +0100

    first commit deploy vercel

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
+
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
 // export default defineConfig({
+//   base: '/76HUNgfdyz0987/',
 //   plugins: [react()],
 // })
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/76HUNgfdyz0987/',
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === 'production' ? '/76HUNgfdyz0987/' : '/', // Base dynamique
+  build: {
+    outDir: 'dist',
+  },
+}));
+
