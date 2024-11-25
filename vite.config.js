dbf2dcb36941b876b29f4c7b653a97cc80939fef
franeltovidjale <tovidjalef@gmail.com>
2024-11-25 17:35:18 +0100

    first commit deploy

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/76HUNgfdyz0987/',
+  base: './',
   plugins: [react()],
 })
