9948f703c5563ad37458c2f0a8d04969969accba
franeltovidjale <tovidjalef@gmail.com>
2024-12-04 22:05:20 +0100

    fix: correction des chemins d'assets

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,30 @@
 
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/', 
+// });
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
+});
+
